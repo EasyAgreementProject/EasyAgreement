6057b4da9e102d05b984f2affc9788bd6c0d28ff
Luis <bobo121@hotmail.it>
2019-12-14 22:16:48 +0100

    Script
    Il commit comprende dei file json contenenti i dati di base per una prima popolazione del database. Include due script js, il primo inserisce tutto nel database, mentre reset elimina tutti i dati inseriti dal primo. Non è stato pacchettizato in cartelle in vista di una possibile unione di alcuni file. Gli script sono eseguibili tramite un semplice "node nomescript.js" da terminale.

---
+++ dbpop_script.js
@@ -0,0 +1,72 @@
+var MongoClient = require('mongodb').MongoClient;
+
+//Database URL
+const url="mongodb://localhost:27017/easyagreement";
+
+//Database name
+const dbName="easyagreement";
+
+MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
+   
+    if(err) throw err;
+    console.log("Connected successfully to server!");
+    var dbo= db.db(dbName);
+
+    
+    const fs = require('fs');
+        let studentData = fs.readFileSync('dbpop_student.json');
+        let adminData = fs.readFileSync('dbpop_admin.json');
+        let actutorData = fs.readFileSync('dbpop_actutor.json');
+        let extTutorData = fs.readFileSync('dbpop_exttutor.json');
+        let hostOrgData = fs.readFileSync('dbpop_hostorg.json');
+
+
+
+        let students = JSON.parse(studentData);
+        let admin = JSON.parse(adminData);
+        let actutor = JSON.parse(actutorData);
+        let exttutor = JSON.parse(extTutorData);
+        let hostorg = JSON.parse(hostOrgData);
+        dbo.collection("Student").insertMany(students,function(err, result) {
+
+            if(err) throw err;
+            console.log("Succesfully inserted into database "+result.insertedCount+" students");
+
+        });
+
+        dbo.collection("Administrator").insertMany(admin,function(err, result) {
+
+            if(err) throw err;
+            console.log("Succesfully inserted into database "+result.insertedCount+" admins");
+
+        });
+
+        dbo.collection("AcademicTutor").insertMany(actutor,function(err, result) {
+
+            if(err) throw err;
+            console.log("Succesfully inserted into database "+result.insertedCount+" academic tutors");
+
+        });
+
+        dbo.collection("ExternalTutor").insertMany(exttutor,function(err, result) {
+
+            if(err) throw err;
+            console.log("Succesfully inserted into database "+result.insertedCount+" external tutors");
+
+        });
+
+        dbo.collection("HostOrganization").insertMany(hostorg,function(err, result) {
+
+            if(err) throw err;
+            console.log("Succesfully inserted into database "+result.insertedCount+" host organizations");
+
+        });
+
+
+
+    
+
+
+
+
+});
