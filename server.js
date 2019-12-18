a84665fed8af0dded2d0e0d2b2c7812af6815c64
alessio <alessio@alessio.homenet.telecomitalia.it>
2019-12-18 22:30:01 +0100

    Sviluppo del front-end

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
+
 app.get('/', function (req, res) {
-    res.send("Hello World");
+    res.sendFile('app/views/signup.html',{root: __dirname});
+});
+
+app.post('/signup', function(req, res) {
+  var signupUser=signupControl.signup(req);
 });
 
 app.listen(8080, function () {
-  console.log('EasyAgreement Platform listening on port 3000!');
+  console.log('EasyAgreement Platform listening on port 8080!');
 });
