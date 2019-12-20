e36f0355d12becacf40b33ae46f6d152e6b82996
Veronica <angel.nostale@gmail.com>
2019-12-20 19:11:58 +0100

    added /saveCompilation

--- app/controllers/learningAgreementControl.js
+++ app/controllers/learningAgreementControl.js
@@ -3,42 +3,44 @@ var fs = require('fs');
 var LA = require('../models/learningAgreement.js');
 var learningAgreement = new LA();
 
-exports.compileLaStudent = function() {
+exports.sendLaStudent = function() {
     var sourcePDF = "pdf/Template_LA.pdf";
     var destinationPDF =  "pdf/Filled_LA.pdf";
     var data = {
-        "Header name" : "Veronica Volpicelli",
-        "Last name (s)" : "Volpicelli",
-        "First name (s)": "Veronica",
-        "Date of birth" : "22/04/1996",
-        "Nationality":"Italiana",
-        "Sex [M/F]" : "F",
+        //"Header name" : input[0]+input[1],
+        //"Last name (s)" : input[1],
+        //"First name (s)": input[0],
+        //"Date of birth" : input[2],
+        //"Nationality":input[5],
+        //"Sex [M/F]" : input[4],
         "Academic year1":"19",
         "Academic year2":"20",
-        "Study cycle" : "1st cycle",
+        //"Study cycle" : input[6],
         "Subject area, Code":"Informatica, 05121",
-        "Phone" : "0123456789",
+        //"Phone" : input[3],
         "E-mail" : "v.volpicelli4@studenti.unisa.it",
         "Sending Departement":"Informatica",
+        "Contact person name":"Filomena Ferrucci",
+        "Contact person E-mail / phone": "958304953",
         "Contact person name / position":"Filomena Ferrucci",
-        "Contact person Email / Phone":"f.ferrucci@unisa.it",
+        "Receiving contact person e-mail phone":"f.ferrucci@unisa.it",
         "Name Sector":"Non lo so",
         "Receiving Department":"Boh",
         "Address, website":"www.nonoloso.it",
         "Country":"Nessuna",
         "Size of enterprise":"Ma che è",
         "Mentor name / position":"Michela Bertolotto",
-        "Mentor Email / Phone":"9876543210",
+        "Mentor e-mail / phone":"9876543210",
         "from":"12/2019",
         "till":"06/2020",
         "Number of working hours for week":"8",
-        "Traineeship Title":"Non ho voglia di fare l'università",
+        "Traineeship title":"Non ho voglia di fare l'università",
         "Detailed programme of the traineeship period": "Non ho fatto un c.... per tutto il periodo",
         "Knowledge, skill and competences to be acquired by the trainee at the end of the traineeship":"Imparare a fare la pizza",
         "Monitoring plan":"Non so cosa dovrebbe essere",
         "Evaluation plan":"Come per il monitoring plan",
         "language competence":"english",
-        "The trainee signature": "Veronica Volpicelli",
+        //"The trainee signature": input[0]+input[1],
         "The trainee date":"08/12/2019"
     };
   
@@ -59,6 +61,71 @@ exports.compileLaStudent = function() {
                 var insertLearningAgreementPr = LA.insertLearningAgreement(learningAgreement);
                 insertLearningAgreementPr.then(function() {
                     fulfill(learningAgreement);
+                });
+                
+            }
+        });
+    })
+}
+
+exports.saveLaStudent = function() {
+    var sourcePDF = "pdf/Template_LA.pdf";
+    var destinationPDF =  "pdf/Filled_LA.pdf";
+    var data = {
+        //"Header name" : input[0]+input[1],
+        //"Last name (s)" : input[1],
+        //"First name (s)": input[0],
+        //"Date of birth" : input[2],
+        //"Nationality":input[5],
+        //"Sex [M/F]" : input[4],
+        "Academic year1":"19",
+        "Academic year2":"20",
+        //"Study cycle" : input[6],
+        "Subject area, Code":"Informatica, 05121",
+        //"Phone" : input[3],
+        "E-mail" : "v.volpicelli4@studenti.unisa.it",
+        "Sending Departement":"Informatica",
+        "Contact person name":"Filomena Ferrucci",
+        "Contact person E-mail / phone": "958304953",
+        "Contact person name / position":"Filomena Ferrucci",
+        "Receiving contact person e-mail phone":"f.ferrucci@unisa.it",
+        "Name Sector":"Non lo so",
+        "Receiving Department":"Boh",
+        "Address, website":"www.nonoloso.it",
+        "Country":"Nessuna",
+        "Size of enterprise":"Ma che è",
+        "Mentor name / position":"Michela Bertolotto",
+        "Mentor e-mail / phone":"9876543210",
+        "from":"12/2019",
+        "till":"06/2020",
+        "Number of working hours for week":"8",
+        "Traineeship title":"Non ho voglia di fare l'università",
+        "Detailed programme of the traineeship period": "Non ho fatto un c.... per tutto il periodo",
+        "Knowledge, skill and competences to be acquired by the trainee at the end of the traineeship":"Imparare a fare la pizza",
+        "Monitoring plan":"Non so cosa dovrebbe essere",
+        "Evaluation plan":"Come per il monitoring plan",
+        "language competence":"english",
+        //"The trainee signature": input[0]+input[1],
+        "The trainee date":"08/12/2019"
+    };
+  
+    return new Promise(function (fulfill, reject) {          
+        pdfFiller.fillForm(sourcePDF, destinationPDF, data, function(err) { 
+            if (err)
+                throw err;
+            else {
+                console.log("PDF create successfully!");
+                //send Filled PDF to Client side
+                var file = fs.createReadStream('pdf/Filled_LA.pdf');
+                learningAgreement.setFilling(data);
+                learningAgreement.setDocument(null);
+                learningAgreement.setStudentID(data["E-mail"]);
+                learningAgreement.setState(null);
+                learningAgreement.setDate(data["The trainee date"]);
+              
+                var insertLearningAgreementPr = LA.insertLearningAgreement(learningAgreement);
+                insertLearningAgreementPr.then(function() {
+                    fulfill(file);
                 });
                 
             }
--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser')
 var learningAgreementControl = require('./app/controllers/learningAgreementControl');
 
 //Loading static files from CSS and Bootstrap module
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
+// to support JSON-encoded bodies and URL-encoded bodies
+app.use(bodyParser.json());       
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(express.json());
+app.use(express.urlencoded()); 
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + "/app/views/compileLAStudent.html"))
 });
 
 app.get('/compileStudent', function(req, res) {
-    //TO DO send parameters from rquest to controller
-    var compileStudent = learningAgreementControl.compileLaStudent();
-    compileStudent.then(function(la) {
+   // var data = [req.body.inputName, req.body.inputSurname, req.body.inputDate, req.body.inputTelephone, req.body.radio1, req.body.nationality, req.body.inputStudyCicle, req.body.inputAcademicYear, req.body.inputCode, req.body.inputEmail];
+    var sendStudent = learningAgreementControl.sendLaStudent();
+    sendStudent.then(function(la) {
         var document = la.document;
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'attachment; filename = LA.pdf');
         document.pipe(res)
+    });
+});
+
+app.get('/saveCompilation', function (req, res) {
+    var saveStudent = learningAgreementControl.saveLaStudent();
+    saveStudent.then(function(file) {
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename = LA.pdf');
+        file.pipe(res)
     });
 });
 
