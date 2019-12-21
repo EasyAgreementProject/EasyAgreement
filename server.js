525ce8119a32330e563a9703dca59355ec941bee
MMeMM <MeM@MBP-di-Marco.lan>
2019-12-21 18:39:11 +0100

    Bug fixed compileLAStudent view

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser')
 var learningAgreementControl = require('./app/controllers/learningAgreementControl');
+var cookieParser = require('cookie-parser');
 
 //Loading static files from CSS and Bootstrap module
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -24,11 +26,12 @@ app.get('/fillForm', function(req, res) {
 });
 
 app.get('/compileStudent', function(req, res) {
-    var data = [req.query.inputName, req.query.inputSurname, req.query.inputDate, req.query.inputTelephone, req.query.radio1, req.query.nationality, req.query.inputStudyCicle, 
-                req.query.inputAcademicYear1, req.query.inputAcademicYear2, req.query.inputSubjectCode, req.query.inputEmail. req.query.inputDepartmentSending, req.query.inputContactSending, 
-                req.query.inputNameSector, req.query.inputDepartmentReciving, req.query.inputWebSite, req.query.inputCountry, req.query.inputSizeEnterprise, req.query.inputContactReciving, 
-                req.query.inputMentor, req.query.inputMentorInfo, req.query.inputDateFrom, req.query.inputDateTo, req.query.inputHourWork, req.query.inputTitle, req.query.inputDetailed, 
-                req.query.inputKnoledge, req.query.inputMonitoring, req.query.inputEvaluation, req.query.inputLenguage, req.query.inputLenguageLevel, req.query.inputDateCompilation];
+    var data = [req.query.inputName, req.query.inputSurname, req.query.inputDate, req.query.inputTelephone, req.query.radio1, req.query.nationality, req.query.inputStudyCicle,
+        req.query.inputAcademicYear1, req.query.inputAcademicYear2, req.query.inputSubjectCode, req.query.inputEmail.req.query.inputDepartmentSending, req.query.inputContactSending,
+        req.query.inputNameSector, req.query.inputDepartmentReciving, req.query.inputWebSite, req.query.inputCountry, req.query.inputSizeEnterprise, req.query.inputContactReciving,
+        req.query.inputMentor, req.query.inputMentorInfo, req.query.inputDateFrom, req.query.inputDateTo, req.query.inputHourWork, req.query.inputTitle, req.query.inputDetailed,
+        req.query.inputKnoledge, req.query.inputMonitoring, req.query.inputEvaluation, req.query.inputLenguage, req.query.inputLenguageLevel, req.query.inputDateCompilation
+    ];
     var sendStudent = learningAgreementControl.sendLaStudent(data);
     sendStudent.then(function(la) {
         var document = la.document;
@@ -42,11 +45,12 @@ app.get('/compileStudent', function(req, res) {
 });
 
 app.get('/saveCompilation', function(req, res) {
-    var data = [req.query.inputName, req.query.inputSurname, req.query.inputDate, req.query.inputTelephone, req.query.radio1, req.query.nationality, req.query.inputStudyCicle, 
-        req.query.inputAcademicYear1, req.query.inputAcademicYear2, req.query.inputSubjectCode, req.query.inputEmail. req.query.inputDepartmentSending, req.query.inputContactSending, 
-        req.query.inputNameSector, req.query.inputDepartmentReciving, req.query.inputWebSite, req.query.inputCountry, req.query.inputSizeEnterprise, req.query.inputContactReciving, 
-        req.query.inputMentor, req.query.inputMentorInfo, req.query.inputDateFrom, req.query.inputDateTo, req.query.inputHourWork, req.query.inputTitle, req.query.inputDetailed, 
-        req.query.inputKnoledge, req.query.inputMonitoring, req.query.inputEvaluation, req.query.inputLenguage, req.query.inputLenguageLevel, req.query.inputDateCompilation];
+    var data = [req.query.inputName, req.query.inputSurname, req.query.inputDate, req.query.inputTelephone, req.query.radio1, req.query.nationality, req.query.inputStudyCicle,
+        req.query.inputAcademicYear1, req.query.inputAcademicYear2, req.query.inputSubjectCode, req.query.inputEmail.req.query.inputDepartmentSending, req.query.inputContactSending,
+        req.query.inputNameSector, req.query.inputDepartmentReciving, req.query.inputWebSite, req.query.inputCountry, req.query.inputSizeEnterprise, req.query.inputContactReciving,
+        req.query.inputMentor, req.query.inputMentorInfo, req.query.inputDateFrom, req.query.inputDateTo, req.query.inputHourWork, req.query.inputTitle, req.query.inputDetailed,
+        req.query.inputKnoledge, req.query.inputMonitoring, req.query.inputEvaluation, req.query.inputLenguage, req.query.inputLenguageLevel, req.query.inputDateCompilation
+    ];
     var saveStudent = learningAgreementControl.saveLaStudent(data);
     saveStudent.then(function(file) {
         res.setHeader('Content-Type', 'application/pdf');
