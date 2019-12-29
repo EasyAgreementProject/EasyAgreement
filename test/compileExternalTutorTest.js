6197b84dcd69b01f11f97e2702317a868c47ef23
Marco <m.ciano4@studenti.unisa.it>
2019-12-29 18:46:47 +0100

    Added compileExternalTutorTest.js

--- test/compileExternalTutorTest.js
+++ test/compileExternalTutorTest.js
@@ -5,80 +5,80 @@ describe('Field test for learningAgreementControl', function(){
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.1', function(){
         var input = ['SA'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.2', function(){
         var input = ['Si', ''];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
     
     it('Testing method sendLaExternalTutor - TC_LAM_3.3', function(){
         var input = ['Si', 'a'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.4', function(){
         var input = ['Si', '300', 'SA'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.5', function(){
         var input = ['Si', '300', 'Si', ''];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.6', function(){
         var input = ['Si', '300', 'Si', '&$£'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.7', function(){
         var input = ['Si', '300', 'Si', 'Buoni Pasto', 'SA'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.8', function(){
         var input = ['Si', '300', 'Si', 'Buoni Pasto', 'Si', '10'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
     it('Testing method sendLaExternalTutor - TC_LAM_3.9', function(){
         var input = ['Si', '300', 'Si', 'Buoni Pasto', 'Si', '&%"£'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNull(la);
         });
     });
 
-    it('Testing method sendLaExternalTutor - TC_LAM_3.9', function(){
+    it('Testing method sendLaExternalTutor - TC_LAM_3.10', function(){
         var input = ['Si', '300', 'Si', 'Buoni Pasto', 'Si', '5'];
-        var sendExternalTutor = compileControl.sendExternalTutor(input);
-        sendExternalTutor.then(function(la) {
+        var SendLaExternalTutor = compileControl.SendLaExternalTutor(input);
+        SendLaExternalTutor.then(function(la) {
             assert.isNotNull(la);
         });
     });
