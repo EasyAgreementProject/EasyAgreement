6a5154b89d718284ac8ac3f48072c5f579945bea
rveneruso <r.veneruso1@studenti.unisa.it>
2020-01-04 13:04:46 +0100

    chatTest.js

--- test/chatTest.js
+++ test/chatTest.js
@@ -2,62 +2,51 @@ var chai= require('chai');
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 var expect=chai.expect;
+var messID;
 
 describe('Field test for messageControl', function() {
     
-    it('Testing getMessages', function(done) {
-        chai
-            .request('http://localhost:8080')
-            .post('/getMessages')
-            .send({sender: "d.devito@studenti.unisa.it", recipient:"g.musso@unisa.it" })
-            .end(function(err,res) {
-                if (err) return done(err);
-                expect(res).to.be.json;
-                done();
-            });
-    
-    });
 
-    it('Testing updateMessage', function(done) {
-        chai
-            .request('http://localhost:8080')
-            .post('/updateMessage')
-            .send({messageID: "5e09f2b0de2932303ad5078d", text:"ciao"})
-            .end(function(err,res) {
-                if (err) return done(err);
-                expect(res).to.be.json;
-                done();
-            });
-    
-    });
-
-    it('Testing removeMessage', function(done) {
-        chai
-            .request('http://localhost:8080')
-            .post('/removeMessage')
-            .send({messageID: "5e09f2b0de2932303ad5078d" })
-            .end(function(err,res) {
-                if (err) return done(err);
-                expect(res).to.be.json;
-                done();
-            });
-    
-    });
-
-    it('Testing saveMessage', function(done) {
+    it.only('Testing saveMessage', function(done) {
         chai
             .request('http://localhost:8080')
             .post('/saveMessage')
             .send({message:{senderID: "d.devito@studenti.unisa.it", recipientID: "g.musso@unisa.it", text: "fratmmm", date:{hour:"12", minutes:"20", seconds:"10", day:"24", months:"12", year:"2019"}} })
             .end(function(err,res) {
                 if (err) return done(err);
+                messID=expect(res).to.be.json;
+                done();
+            });
+    
+    });
+
+    it.only('Testing getMessages', function(done) {
+        chai
+            .request('http://localhost:8080')
+            .post('/getMessages')
+            .send({messID})
+            .end(function(err,res) {
+                if (err) return done(err);
                 expect(res).to.be.json;
                 done();
             });
     
     });
 
-    it('Testing getContacts 1.1', function(done) {
+    it.only('Testing updateMessage', function(done) {
+        chai
+            .request('http://localhost:8080')
+            .post('/updateMessage')
+            .send({messID})
+            .end(function(err,res) {
+                if (err) return done(err);
+                expect(res).to.be.json;
+                done();
+            });
+    
+    });
+
+    it.only('Testing getContacts 1.1', function(done) {
         chai
             .request('http://localhost:8080')
             .post('/getContacts')
@@ -70,7 +59,7 @@ describe('Field test for messageControl', function() {
     
     });
 
-    it('Testing getContacts 1.2', function(done) {
+    it.only('Testing getContacts 1.2', function(done) {
         chai
             .request('http://localhost:8080')
             .post('/getContacts')
@@ -83,7 +72,7 @@ describe('Field test for messageControl', function() {
     
     });
 
-    it('Testing getContacts 1.3', function(done) {
+    it.only('Testing getContacts 1.3', function(done) {
         chai
             .request('http://localhost:8080')
             .post('/getContacts')
@@ -96,11 +85,24 @@ describe('Field test for messageControl', function() {
     
     });
 
-    it('Testing searchUser', function(done) {
+    it.only('Testing searchUser', function(done) {
         chai
             .request('http://localhost:8080')
             .post('/searchUser')
             .send({ type: "externalTutor" , search: "Marco"})
+            .end(function(err,res) {
+                if (err) return done(err);
+                expect(res).to.be.json;
+                done();
+            });
+    
+    });
+
+    it.only('Testing removeMessage', function(done) {
+        chai
+            .request('http://localhost:8080')
+            .post('/removeMessage')
+            .send({messID })
             .end(function(err,res) {
                 if (err) return done(err);
                 expect(res).to.be.json;
