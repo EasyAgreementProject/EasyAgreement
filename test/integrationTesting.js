900537cb84326ed1824ae284593cda15cbb60bd7
Veronica <angel.nostale@gmail.com>
2020-01-09 23:25:14 +0100

    Added tests

--- test/integrationTesting.js
+++ test/integrationTesting.js
@@ -11,6 +11,28 @@ describe('Field test for server.js', function() {
     it.only('Testing get compileLAStudent.html', function(done) {
         request(app).get("/compileLAStudent.html")
             .expect(200)
+            .expect(/Compilazione Learning Agreement/);
+            done();
+    });
+
+    it.only('Testing get viewLA.html', function(done) {
+        request(app).get("/viewLA.html")
+            .expect(200)
+            .expect(/Learning Agreement/);
+            done();
+    });
+
+    it.only('Testing get gestioneDocumenti.html', function(done) {
+        request(app).get("/gestioneDocumenti.html")
+            .expect(200)
+            .expect(/Gestione Documenti/);
+            done();
+    });
+
+    it.only('Testing get login.html', function(done) {
+        request(app).get("/")
+            .expect(200)
+            .expect(/Login/);
             done();
     });
 });
