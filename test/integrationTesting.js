75b547da94d850b557a08672bbdc3dfcedf68cda
Veronica <angel.nostale@gmail.com>
2020-01-10 17:43:58 +0100

    Fixed input for compileStudent

--- test/integrationTesting.js
+++ test/integrationTesting.js
@@ -146,10 +146,43 @@ describe('Field test for server.js', function() {
         chai
             .request('http://localhost:8080')
             .post('/compileStudent')
-            .send({inputName: 'inputVeronica', inputSurname: 'Volpicelli', inputDate: '22/04/1996', inputTelephone:'123456789', radio1:'F', nationality:'Italiana', inputStudyCycle:'1st Cycle', inputAcademicYear1:'19', inputAcademicYear2:'20', inputSubjectCode:'Informatica, 05121', inputEmail:'v.volpicelli4@studenti.unisa.it', inputDepartmentSending:'Informatica', inputContactName:'Filomena Ferrucci',
-            inputContactSending:'f.ferrucci@unisa.it 123456789', inputNameSector:'Informatica', inputDepartmentReciving:'Google', inputAddressWebSite:'Via delle Foglie, 4 www.google.it', inputCountry:'America', inputSizeEnterprise:'300-500', inputContactReciving:'Filomena Ferrucci - Responsabile', inputMentor:'Michela Bertolotto - Direttrice',
-            inputMentorInfo:'m.berto@gmail.com 0987654321', inputDateFrom:'06/2020', inputDateTo:'06/2021', inputHourWork:'8', inputTitle:'Us Academy', inputDetailed:'Learning of Modern and Advanced Technologies', inputKnowledge:'Use of MongoDB, Mocha and Chai', inputMonitoring:'Weekly meeting',
-            inputEvaluation:'Knowledge of the tools', inputLenguage:'english',inputLenguageLevel:'A1P' })
+            .send({ 
+                inputAddress:"Via delle Foglie, 4",
+                inputWebSite:"www.google.it",
+                inputContactRecivingName:"Filomena Ferrucci",
+                inputContactRecivingPosition:"Responsabile",
+                inputMentorName:"Michela Bertolotto",
+                inputMentorPosition:"Direttrice",
+                inputName:"Veronica",
+                inputSurname:"Volpicelli", 
+                inputDate:"22/04/1996", 
+                inputTelephone:"123456789", 
+                radio1:"F",
+                nationality:"Italiana", 
+                inputStudyCycle:"1st Cycle",
+                inputAcademicYear1:"19",
+                inputAcademicYear2:"20",
+                inputSubjectCode:"Informatica, 05121",
+                inputEmail:"v.volpicelli1@studenti.unisa.it",
+                inputDepartmentSending:"Informatica", 
+                inputContactName:"Filomena Ferrucci", 
+                inputContactSending:"f.ferrucci@unisa.it 123456789",
+                inputNameSector:"Informatica", 
+                inputDepartmentReciving:"Google", 
+                inputCountry:"America", 
+                inputSizeEnterprise:"300-500",
+                inputMentorInfo:"m.berto@gmail.com 0987654321",
+                inputDateFrom:"06/2020",
+                inputDateTo:"06/2021", 
+                inputHourWork:"8", 
+                inputTitle:"Us Academy", 
+                inputDetailed:"Learning of Modern and Advanced Technologies",
+                inputKnowledge:"Use of MongoDB, Mocha and Chai",
+                inputMonitoring:"Weekly meeting", 
+                inputEvaluation:"Knowledge of the tools", 
+                inputLenguage:"english", 
+                inputLenguageLevel:"B2"
+            })
             .end(function(err, res){
                 if(err) return done(err)
                 expect(res).to.not.be.null
