db350340fd746eb110fb444e2f9d9b34ca032e96
marcol8b <marcol8b@DESKTOP-R3RAMFB.home.com>
2020-01-10 16:24:40 +0100

    integr testing

--- test/integrationTesting.js
+++ test/integrationTesting.js
@@ -30,4 +30,35 @@ describe('Field test for server.js', function() {
                 done()
             })
     })
+
+    it('Testing to compileLaStudent', function(done){
+        chai
+            .request('http://localhost:8080')
+            .get('/compileLAStudent.html')
+            .end(function(err, res){
+                if(err) return done(err)
+                console.log(res)
+                expect(res).to.not.be.null
+                done()
+            })
+    })
+
+  
+
+    it('Testing to fillForm', function(done){
+        chai
+            .request('http://localhost:8080')
+            .post('/login')
+            .send({username:"d.devito@studenti.unisa.it", password: "DannyDeVito1"})
+            .end(function(err, res){
+                if(err) return done(err)
+               chai
+                  .request('http://localhost:8080')
+                  .post('/login')
+                  .end(function(err, res){
+                         expect(res).to.not.be.null
+                         done()
+                  })
+            })
+    })
 })
